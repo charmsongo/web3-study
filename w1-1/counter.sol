f3198002177c4226813493c1e88ca8ba8c14580f
charmsongo <charmsongo@163.com>
2025-05-18 15:23:23 +0800

    counter  test add

---
+++ w1-1/counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Counter{
+    uint public counter;
+
+    constructor(){
+        counter = 0;
+    }
+
+    function count() public{
+        counter = counter + 1;
+    }
+}
+
